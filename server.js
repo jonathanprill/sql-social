092729457e1a762b59b0df61f71f56dfdd54e5cc
jonathanprill <jonathantprill@gmail.com>
2022-06-07 17:07:55 -0500

    adds connection to mysql

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,7 +7,16 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'WellyisC00l!',
+        database: 'social'
+    },
+    console.log('Connected to the social database.')
+);
 
 
 // Default response for any other request (Not Found)
