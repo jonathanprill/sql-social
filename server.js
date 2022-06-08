a72cb419312f43a4bd93e10a878f0137916b8de2
jonathanprill <jonathantprill@gmail.com>
2022-06-08 11:37:56 -0500

    adds routes to api

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const inputCheck = require('./utils/inputCheck');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
@@ -17,6 +18,95 @@ const db = mysql.createConnection(
     },
     console.log('Connected to the social database.')
 );
+
+// Get all users
+app.get('/api/users', (req, res) => {
+    const sql = `SELECT * FROM users`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+// Get a single user
+app.get('/api/user/:id', (req, res) => {
+    const sql = `SELECT * FROM users WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+// Delete a user
+app.delete('/api/user/:id', (req, res) => {
+    const sql = `DELETE FROM users WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.statusMessage(400).json({ error: res.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'user not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+
+// Create a user
+// const sql = `INSERT INTO users (id, username, clan_connected) 
+//               VALUES (?,?,?)`;
+// const params = [1, 'jonyprill', 1];
+
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+// Create a user
+app.post('/api/user', ({ body }, res) => {
+    const errors = inputCheck(body, 'username', 'clan_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    const sql = `INSERT INTO users (username, clan_connected)
+  VALUES (?,?)`;
+    const params = [body.username, body.clan_connected];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
 
 
 // Default response for any other request (Not Found)
