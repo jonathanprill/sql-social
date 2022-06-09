7419c0e32a265720ff795d11b1ad90de932b1141
jonathanprill <jonathantprill@gmail.com>
2022-06-09 11:19:25 -0500

    adds follower routes

--- routes/apiRoutes/followerRoutes.js
+++ routes/apiRoutes/followerRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require('../../db/connection');
 const inputCheck = require('../../utils/inputCheck');
 
+//get all followers
 router.get('/followers', (req, res) => {
     const sql = `SELECT * FROM followers ORDER BY username`;
 
@@ -15,6 +16,93 @@ router.get('/followers', (req, res) => {
             message: 'success',
             data: rows,
         });
+    });
+});
+
+//get a single follower
+router.get('/follower/:id', (req, res) => {
+    const sql = `SELECT * FROM followers WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+// create a follower
+router.post('/follower', ({ body }, res) => {
+    // Data validation
+    const errors = inputCheck(body, 'username', 'email');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    const sql = `INSERT INTO followers (username, email) VALUES (?,?)`;
+    const params = [body.username, body.email];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
+//Uodate a followers email
+router.put('/follower/:id', (req, res) => {
+    // Data validation
+    const errors = inputCheck(req.body, 'email');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    const sql = `UPDATE followers SET email = ? WHERE id = ?`;
+    const params = [req.body.email, req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+        } else if (!result.affectedRows) {
+            res.json({ message: 'no follower with that id' });
+        } else {
+            res.json({
+                message: 'success',
+                data: req.body,
+                changes: result.affectedRows
+            });
+        }
+    });
+});
+
+// Delete follower
+router.delete('/follower/:id', (req, res) => {
+    const sql = `DELETE FROM followers WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, req.params.id, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: res.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Follower not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
     });
 });
 
